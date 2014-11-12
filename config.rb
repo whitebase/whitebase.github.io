f133970b08ad74582cda8b721615b2bfba5c0a74
gen <gen@spfdesign.jp>
2014-11-12 16:07:06 +0900

    初期ファイル

---
+++ config.rb
@@ -0,0 +1,26 @@
+require 'compass-h5bp'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "_/css"
+sass_dir = "_scss"
+images_dir = "_/img"
+javascripts_dir = "_/js"
+fonts_dir = "_/font"
+
+output_style = :expanded
+#output_style = :compressed
+
+
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
+
+#sass_options = { :debug_info => true }
